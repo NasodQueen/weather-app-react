ae6130df55a629d27053aef451903e4add751551
NasodQueen <119695089+NasodQueen@users.noreply.github.com>
2023-06-24 19:34:25 +0200

    Replaced the prop data with a placeholder text for now

--- src/HeaderData.js
+++ src/HeaderData.js
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 
 import Form from "./Form";
 
-export default function HeaderData(props) {
+export default function HeaderData() {
   return (
     <div className="HeaderData">
       <div className="header-city-weather-position">
         <h1 className="city-name">New York</h1>
         <h2 className="date-and-time">Tuesday, 16:24</h2>
         <h2>
-          <span className="header-weather-text">{props.description}</span> |
+          <span className="header-weather-text">Haze</span> |
           <span className="header-temperature-degrees">20</span>
           <span className="temp-unit">°C</span>
         </h2>
