a59c27e8fa2797613ea5d6b3f1921ac5cbf4d977
NasodQueen <119695089+NasodQueen@users.noreply.github.com>
2023-07-01 11:42:01 +0200

    Imported bootstrap

--- src/Header.js
+++ src/Header.js
@@ -14,19 +14,15 @@ export default function Header(props) {
         <HeaderNotice code={props.data.icon} />
       </div>
       <div className="HeaderData">
-        <div className="HeaderData">
-          <div className="header-city-weather-position">
-            <h1 className="city-name">{props.data.city}</h1>
-            <h2 className="date-and-time">
-              <DateFormat date={props.data.date} />
-            </h2>
-            <h2>
-              <span className="header-weather-text">
-                {props.data.description}
-              </span>{" "}
-              | <WeatherTemperature celsius={props.data.temperature} />
-            </h2>
-          </div>
+        <div className="header-city-weather-position">
+          <h1 className="city-name">{props.data.city}</h1>
+          <h2 className="date-and-time">
+            <DateFormat date={props.data.date} />
+          </h2>
+          <h2>
+            <span className="text-capitalize">{props.data.description}</span> |{" "}
+            <WeatherTemperature celsius={props.data.temperature} />
+          </h2>
         </div>
       </div>
       <div className="HeaderIcon">
