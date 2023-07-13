cfbf2d0d700c20ead6ac534c04416ff181b3b822
NasodQueen <119695089+NasodQueen@users.noreply.github.com>
2023-07-13 15:20:29 +0200

    Created new forecast component for easier formatting and reusing for all forecast days

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,35 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 import WeatherIcon from "./WeatherIcon";
+import ForecastDay from "./ForecastDay";
 
 import "./Forecast.css";
 
 export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  const apiKey = "57b2c40fdae71a6ba41d72685e3226e2";
-  let lat = props.coordinates.lat;
-  let lon = props.coordinates.lon;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="ForecastSingle">
-      <div className="card col">
-        <div className="card-body">
-          <h5 className="card-title">Thursday</h5>
-          <WeatherIcon code="01d" size={36} />
-          <p className="card-text">
-            <span className="forecast-max-temp">26</span>
-            <span className="temp-unit">°C</span> |
-            <span className="forecast-min-temp">11</span>
-            <span className="temp-unit">°C</span>
-          </p>
-        </div>
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <ForecastDay data={forecast[0]} />
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "57b2c40fdae71a6ba41d72685e3226e2";
+    let lat = props.coordinates.lat;
+    let lon = props.coordinates.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 }
