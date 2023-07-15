ea63aae22ae93a46f430204ae25667b0ddeee34f
NasodQueen <119695089+NasodQueen@users.noreply.github.com>
2023-07-15 23:32:00 +0200

    Updated some of the icons

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,5 +1,7 @@
 import React from "react";
-import ReactAnimatedWeather from "react-animated-weather";
+// import ReactAnimatedWeather from "react-animated-weather";
+
+import { ReactSkycon } from "react-skycons-extended";
 
 import "./Header.css";
 import "./index.css";
@@ -14,12 +16,12 @@ export default function WeatherIcon(props) {
     "03n": "PARTLY_CLOUDY_NIGHT",
     "04d": "CLOUDY",
     "04n": "CLOUDY",
-    "09d": "RAIN",
-    "09n": "RAIN",
+    "09d": "SHOWERS_DAY",
+    "09n": "SHOWERS_NIGHT",
     "10d": "RAIN",
     "10n": "RAIN",
-    "11d": "RAIN",
-    "11n": "RAIN",
+    "11d": "THUNDER_SHOWERS_DAY",
+    "11n": "THUNDER_SHOWERS_NIGHT",
     "13d": "SNOW",
     "13n": "SNOW",
     "50d": "FOG",
@@ -27,7 +29,7 @@ export default function WeatherIcon(props) {
   };
 
   return (
-    <ReactAnimatedWeather
+    <ReactSkycon
       icon={codeMapping[props.code]}
       color="#015c92"
       size={props.size}
