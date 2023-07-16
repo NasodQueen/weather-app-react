f070a9d0d6a1d6c1ac9766eab51096b7f2714ce1
NasodQueen <119695089+NasodQueen@users.noreply.github.com>
2023-07-16 23:20:10 +0200

    Updated icon selection array, plus icon color (kind of)
    Attempt at getting the icon color correct has been made, currently only limited functionality (doesn't update with theme change)

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,39 +1,53 @@
-import React from "react";
-// import ReactAnimatedWeather from "react-animated-weather";
+import React, { useState, useEffect } from "react";
 
 import { ReactSkycon } from "react-skycons-extended";
 
 import "./Header.css";
 import "./index.css";
 
+const codeMapping = [
+  {
+    index: "01d",
+    icon: "CLEAR_DAY",
+    color: "var(--first-color)",
+  },
+  { index: "01n", icon: "CLEAR_NIGHT", color: "var(--first-color)" },
+
+  { index: "02d", icon: "PARTLY_CLOUDY_DAY", color: "var(--first-color)" },
+  { index: "02n", icon: "PARTLY_CLOUDY_NIGHT", color: "var(--first-color)" },
+  { index: "03d", icon: "PARTLY_CLOUDY_DAY", color: "var(--first-color)" },
+  { index: "03n", icon: "PARTLY_CLOUDY_NIGHT", color: "var(--first-color)" },
+  { index: "04d", icon: "CLOUDY", color: "var(--first-color)" },
+  { index: "04n", icon: "CLOUDY", color: "var(--first-color)" },
+  { index: "09d", icon: "SHOWERS_DAY", color: "var(--first-color)" },
+  { index: "09n", icon: "SHOWERS_NIGHT", color: "var(--first-color)" },
+  { index: "10d", icon: "RAIN", color: "var(--first-color)" },
+  { index: "10n", icon: "RAIN", color: "var(--first-color)" },
+  { index: "11d", icon: "THUNDER_SHOWERS_DAY", color: "var(--first-color)" },
+  { index: "11n", icon: "THUNDER_SHOWERS_NIGHT", color: "var(--first-color)" },
+  { index: "13d", icon: "SNOW", color: "var(--first-color)" },
+  { index: "13n", icon: "SNOW", color: "var(--first-color)" },
+  { index: "50d", icon: "FOG", color: "var(--first-color)" },
+  { index: "50n", icon: "FOG", color: "var(--first-color)" },
+];
+
 export default function WeatherIcon(props) {
-  const codeMapping = {
-    "01d": "CLEAR_DAY",
-    "01n": "CLEAR_NIGHT",
-    "02d": "PARTLY_CLOUDY_DAY",
-    "02n": "PARTLY_CLOUDY_NIGHT",
-    "03d": "PARTLY_CLOUDY_DAY",
-    "03n": "PARTLY_CLOUDY_NIGHT",
-    "04d": "CLOUDY",
-    "04n": "CLOUDY",
-    "09d": "SHOWERS_DAY",
-    "09n": "SHOWERS_NIGHT",
-    "10d": "RAIN",
-    "10n": "RAIN",
-    "11d": "THUNDER_SHOWERS_DAY",
-    "11n": "THUNDER_SHOWERS_NIGHT",
-    "13d": "SNOW",
-    "13n": "SNOW",
-    "50d": "FOG",
-    "50n": "FOG",
-  };
+  const [color, setColor] = useState(null);
+
+  useEffect(() => {
+    setColor(
+      getComputedStyle(document.documentElement).getPropertyValue(
+        "--first-color"
+      )
+    );
+  }, []); // Run this effect only once on component mount
+
+  // Search the array for an object where the index of the object matches the code passed to the component
+  const matchingCode = codeMapping.find((item) => item.index === props.code);
+
+  const { icon } = matchingCode;
 
   return (
-    <ReactSkycon
-      icon={codeMapping[props.code]}
-      color="#015c92"
-      size={props.size}
-      animate={true}
-    />
+    <ReactSkycon icon={icon} color={color} size={props.size} animate={true} />
   );
 }
